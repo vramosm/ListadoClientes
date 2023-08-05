7d7a2ee3e0966f9ca94f98bc76f8cb046ceb66c7
Víctor Ramos <victor.ramos@optivamedia.com>
2023-08-05 18:33:49 +0200

    Initial Commit

---
+++ ListadoClientes/ListadoClientesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListadoClientesApp.swift
+//  ListadoClientes
+//
+//  Created by optiva on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ListadoClientesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
